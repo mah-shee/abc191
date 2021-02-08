ce5fe5a2b81bc5fa39ad30436284d4814b4ce769
masa <masatoshi.sboard@gmail.com>
2021-02-08 19:03:33 +0900

    Done c

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,17 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        x: usize,
+        mut a: [usize; n],
+    }
+    let mut ans = vec![];
+    for i in 0..n {
+        if a[i] != x {
+            ans.push(a[i]);
+        }
+    }
+    for i in ans.iter() {
+        print!("{} ", i);
     }
 }
