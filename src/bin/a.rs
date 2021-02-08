f7f165b19d018145a93e76f98ae2f2e7e7c2ed49
masa <masatoshi.sboard@gmail.com>
2021-02-08 18:55:36 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,15 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        v:usize,
+        t:usize,
+        s:usize,
+        d:usize,
+    }
+    let point_sec = d as f64 / v as f64;
+    if point_sec < t as f64 || point_sec > s as f64 {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
